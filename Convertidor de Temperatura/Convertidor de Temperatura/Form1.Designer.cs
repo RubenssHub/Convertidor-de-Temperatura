4b5e662168aff8bd51b7dde419e2d3b5e739a8c3
Ruben Catalan Gutierrez <byrubenss@gmail.com>
2019-08-29 16:05:00 -0500

    Convertidor de Temperatura 2.0

--- Convertidor de Temperatura/Convertidor de Temperatura/Form1.Designer.cs
+++ Convertidor de Temperatura/Convertidor de Temperatura/Form1.Designer.cs
@@ -28,20 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtF = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(88, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(403, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Convierte grados Celcius y Farenheit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(35, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grados Celcius:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(35, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Grados Farenheit: ";
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(250, 108);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(236, 20);
+            this.txtC.TabIndex = 3;
+            this.txtC.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.txtC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtC_KeyPress);
+            // 
+            // txtF
+            // 
+            this.txtF.Location = new System.Drawing.Point(250, 192);
+            this.txtF.Name = "txtF";
+            this.txtF.Size = new System.Drawing.Size(236, 20);
+            this.txtF.TabIndex = 4;
+            this.txtF.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            this.txtF.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtF_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(392, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 74);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(552, 251);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(566, 344);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtF);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Convertidor de Temperatura";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtF;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Convertidor de Temperatura/Convertidor de Temperatura/Form1.cs
+++ Convertidor de Temperatura/Convertidor de Temperatura/Form1.cs
@@ -12,9 +12,64 @@ namespace Convertidor_de_Temperatura
 {
     public partial class Form1 : Form
     {
+        int bandera=0;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double grados;
+                //Si escribio en centigrados
+                if (bandera == 1)
+                {
+                    grados = Convert.ToDouble(txtC.Text) * 1.8 + 32.0;
+                    //Mostramos el resultado
+                    txtF.Text = string.Format("{0:F2}", grados);
+                }
+                if (bandera == 2)
+                {
+                    grados = Convert.ToDouble(txtF.Text) - 32.0 * 1.8;
+                    txtC.Text = string.Format("{0:F2}", grados);
+                }
+
+            }
+            catch (FormatException)
+            {
+                txtC.Text = "0.0";
+                txtF.Text = "32.00";
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bandera = 1;
+            if (e.KeyChar == 13)
+            {
+                Button1_Click(sender, e); //Llama la función
+            }
+        }
+
+        private void TxtF_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bandera = 2;
+            if (e.KeyChar == 13)
+            {
+                Button1_Click(sender, e); //Llama la función
+            }
+        }
     }
-}
+};
